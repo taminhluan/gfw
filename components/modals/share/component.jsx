9717d462a83f4d9fc2b31f509ba60eb443615faf
Ed Brett <e.j.a.brett@gmail.com>
2020-07-22 19:18:30 +0200

    use share component

--- components/modals/share/component.jsx
+++ components/modals/share/component.jsx
@@ -1,20 +1,13 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { track } from 'analytics';
 
-import twitterIcon from 'assets/icons/twitter.svg?sprite';
-import facebookIcon from 'assets/icons/facebook.svg?sprite';
+import { Share } from 'gfw-components';
 
-import { Loader } from 'gfw-components';
-
-import Switch from 'components/ui/switch';
-import Button from 'components/ui/button';
-import Icon from 'components/ui/icon';
 import Modal from '../modal';
 
 import './styles.scss';
 
-class Share extends PureComponent {
+class ShareModal extends PureComponent {
   static propTypes = {
     open: PropTypes.bool,
     selected: PropTypes.string,
@@ -27,105 +20,9 @@ class Share extends PureComponent {
     handleCopyToClipboard: PropTypes.func,
   };
 
-  getContent() {
-    const {
-      selected,
-      loading,
-      copied,
-      data,
-      handleFocus,
-      setShareSelected,
-      handleCopyToClipboard,
-    } = this.props;
-    const { title, shareUrl, embedUrl, embedSettings } = data || {};
-    const { width, height } = embedSettings || {};
-
-    const inputValue =
-      selected === 'embed'
-        ? `<iframe width="${width || 670}" height="${
-            height || 490
-          }" frameborder="0" src="${embedUrl}"></iframe>`
-        : shareUrl;
-
-    return (
-      <div className="c-share">
-        <div className="actions">
-          {embedUrl ? (
-            <Switch
-              className="share-switch-tab"
-              theme="theme-switch-light"
-              value={selected}
-              options={[
-                { label: 'LINK', value: 'link' },
-                { label: 'EMBED', value: 'embed' },
-              ]}
-              onChange={
-                selected === 'embed'
-                  ? () => setShareSelected('link')
-                  : () => setShareSelected('embed')
-              }
-            />
-          ) : null}
-          <p className="info">
-            {selected === 'embed'
-              ? 'Click and paste HTML to embed in website'
-              : 'Click and paste link in email or IM'}
-          </p>
-          <div className="input-container">
-            <div className="input">
-              {loading && selected !== 'embed' && (
-                <Loader className="input-loader" />
-              )}
-              <input
-                ref={(input) => {
-                  this.textInput = input;
-                }}
-                type="text"
-                value={!loading ? inputValue : ''}
-                readOnly
-                onClick={handleFocus}
-              />
-            </div>
-            <Button
-              theme="theme-button-medium"
-              className="input-button"
-              onClick={() => handleCopyToClipboard(this.textInput)}
-              disabled={loading}
-            >
-              {copied ? 'COPIED!' : 'COPY'}
-            </Button>
-          </div>
-        </div>
-        <div className="social-container">
-          <Button
-            extLink={`https://twitter.com/intent/tweet?text=${title}&via=globalforests&url=${shareUrl}`}
-            className="social-button"
-            theme="theme-button-light theme-button-grey  square"
-            onClick={() =>
-              track('shareSocial', {
-                label: shareUrl,
-              })}
-          >
-            <Icon icon={twitterIcon} className="twitter-icon" />
-          </Button>
-          <Button
-            extLink={`https://www.facebook.com/sharer.php?u=${shareUrl}`}
-            theme="theme-button-light theme-button-grey square"
-            className="social-button"
-            onClick={() =>
-              track('shareSocial', {
-                label: shareUrl,
-              })}
-          >
-            <Icon icon={facebookIcon} className="facebook-icon" />
-          </Button>
-        </div>
-      </div>
-    );
-  }
-
   render() {
     const { open, setShareOpen, data } = this.props;
+
     return (
       <Modal
         isOpen={open}
@@ -133,10 +30,10 @@ class Share extends PureComponent {
         onRequestClose={() => setShareOpen(false)}
         title={this.props.data && this.props.data.title}
       >
-        {this.getContent()}
+        <Share {...data} token={process.env.BITLY_TOKEN} />
       </Modal>
     );
   }
 }
 
-export default Share;
+export default ShareModal;
--- layouts/my-gfw/component.jsx
+++ layouts/my-gfw/component.jsx
@@ -23,7 +23,7 @@ class MyGFWPage extends PureComponent {
   static propTypes = {
     loggedIn: PropTypes.bool,
     loading: PropTypes.bool,
-    areas: PropTypes.array
+    areas: PropTypes.array,
   };
 
   render() {
@@ -46,8 +46,7 @@ class MyGFWPage extends PureComponent {
             <div className="my-gfw-container">
               <div className="row">
                 {loggedIn && loading && <Loader className="mygfw-loader" />}
-                {loggedIn &&
-                  !loading && (
+                {loggedIn && !loading && (
                   <div className="column small-12">
                     {areas && !!areas.length ? (
                       <div className="row">
@@ -60,28 +59,24 @@ class MyGFWPage extends PureComponent {
                         <div className="column small-12 medium-5">
                           <img
                             className="areas-image"
-                            srcSet={`${DashboardImageLarge} 2x, ${
-                              DashboardImage
-                            } 1x`}
+                            srcSet={`${DashboardImageLarge} 2x, ${DashboardImage} 1x`}
                             src={`${DashboardImage} 1x`}
                             alt="no areas"
                           />
                         </div>
                         <div className="column small-12 medium-6">
-                          <h4>
-                              You haven’t created any Areas of Interest yet
-                          </h4>
+                          <h4>You haven’t created any Areas of Interest yet</h4>
                           <p>
-                              Creating an Area of Interest lets you customize
-                              and perform an in-depth analysis of the area, as
-                              well as receiving email notifications when new
-                              deforestation alerts are available.
+                            Creating an Area of Interest lets you customize and
+                            perform an in-depth analysis of the area, as well as
+                            receiving email notifications when new deforestation
+                            alerts are available.
                           </p>
                           <Button
                             className="learn-btn"
                             link="/map?analysis=eyJzaG93RHJhdyI6ZmFsc2V9&mainMap=eyJzaG93QW5hbHlzaXMiOnRydWV9&map=eyJjZW50ZXIiOnsibGF0IjoyNywibG5nIjoxMn0sImJlYXJpbmciOjAsInBpdGNoIjowLCJ6b29tIjoyfQ%3D%3D&mapPrompts=eyJvcGVuIjp0cnVlLCJzdGVwc0tleSI6ImFyZWFPZkludGVyZXN0VG91ciIsInN0ZXBJbmRleCI6MCwiZm9yY2UiOnRydWV9"
                           >
-                              Learn how
+                            Learn how
                           </Button>
                         </div>
                       </div>
